b97293f2a8005c0d049ddea70b2c05bda4acf97f
Tamanna <tamannahooda28@gmail.com>
2026-07-29 11:21:05 +0530

    First commit

---
+++ test.py/test 2.py
@@ -0,0 +1,125 @@
+#(Ques- 1)create a variable name and store your name?
+# #ans
+# Name = "Tamanna"
+# print(Name)
+
+#(Ques 2)
+#Ans
+# Age = 17
+# print(Age)
+
+#(Ques3)
+#Ans
+# a = 2
+# b = 3
+# print(A+B)
+
+#(Ques 4)
+# Ans
+# a = 3
+# b = 5
+# a,b=b,a
+# print("a=",a)
+# print("b=",b)
+
+#(Ques 5)
+#Ans
+# a = 5
+# b = 10.5
+# c = "Priya"
+# d = True
+# print(type(a),type(b),type(c),type(d))
+
+#(Ques 6)
+#Ans
+a = "85"
+b = int(a)
+print(b)
+print(type(b))
+
+#(Qques 7)
+#Ans
+a = 80
+b = str(a)
+print(b)
+print(type(b))
+
+#(Ques-8)
+#Ans
+height = 6.7
+print(height)
+
+#(Ques 9)
+#Ans
+Name = "Tamanna"
+Age = 17
+City = "Rothak"
+print(Name , Age ,City)
+
+
+
+
+
+#(Ques 10)
+#Ans
+length = float(input("enter a number."))
+breath = float(input("enter a number."))
+area = length*breath
+print("area of rectangle", area)
+
+#(Ques-11)
+#Ans
+radius = int(input("enter a number."))
+area = 3.14*radius**2
+print("area of circle=",area)
+#(Ques 12)
+#Ans
+p = float(input("enter a principle."))
+r = float(input("enter a rate."))
+t = float(input("enter a time."))
+si = (p*r*t)
+print("simpel intrest=",si)
+
+#(Ques 13)
+#Ans
+a = int(input("enter a number."))
+b = int(input("enter a number."))
+c = int(input("enter a number."))
+average = (a+b+c)/3
+print("Average=",average)
+
+#Ques 14
+#Ans
+num = int(input("enter a number."))
+print("square=",num*num)
+print("cube=",num*num*num)
+
+#(Ques15)
+#Ans
+num = int(input("enter a number."))
+if num%2==0:
+    print("even number.")
+else:
+    print("odd numbere.")
+    
+#(Ques16)
+#Ans
+a = int(input("enter a number."))
+b = int(input("enter a number."))
+reminder =- a-(a//b)+b
+print("Reminder=",reminder)
+
+#(Ques 17)
+#Ans
+units = int(input("enter units consumed."))
+bill = units*5
+print("electricity  bill=",bill)
+
+#(ques18)
+#Ans
+celcius = int(input("enter temperature"))
+
+
+
+
+        
